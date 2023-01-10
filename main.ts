45fcc1319c90744e3406fda82bf771e2379f937b
SamS2007 <118993453+SamS2007@users.noreply.github.com>
2023-01-09 23:54:31 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+input.onButtonPressed(Button.A, function () {
+    if (paddle_a.get(LedSpriteProperty.X) < 3) {
+        paddle_a.change(LedSpriteProperty.X, 1)
+        paddle_b.change(LedSpriteProperty.X, 1)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (paddle_a.get(LedSpriteProperty.X) > 0) {
+        paddle_a.change(LedSpriteProperty.X, -1)
+        paddle_b.change(LedSpriteProperty.X, -1)
+    }
+})
+let paddle_b: game.LedSprite = null
+let paddle_a: game.LedSprite = null
+paddle_a = game.createSprite(2, 4)
+paddle_b = game.createSprite(3, 4)
+let ball = game.createSprite(randint(0, 4), 0)
+let direction_y = 1
+let direction_x = randint(-1, 1)
+basic.pause(500)
+basic.forever(function () {
+    ball.change(LedSpriteProperty.X, direction_x)
+    ball.change(LedSpriteProperty.Y, direction_y)
+    if (ball.get(LedSpriteProperty.Y) <= 0) {
+        direction_y = 1
+        direction_x = randint(-1, 1)
+    } else {
+    	
+    }
+})
